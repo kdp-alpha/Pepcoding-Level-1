d954830f0a115d5e77941602395f67e29d98fa1c
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2021-10-10 11:10:37 +0530

    Update Pattern1.java

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        // write ur code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
