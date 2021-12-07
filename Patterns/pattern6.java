1d28aad1c32ef04eba91a173115939a1d99be97a
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2021-12-07 21:36:02 +0530

    Create pattern6.java

---
+++ Patterns/pattern6.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        int st = (n/2)+1;
+        int sp = 1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j = 1;j<=st; j++)
+            {
+                System.out.print("*	");
+            }
+            
+            for(int space = 1; space<=sp; space++)
+            {
+                System.out.print('	');
+            }
+            
+             for(int j = 1;j<=st; j++)
+            {
+                System.out.print("*	");
+            }
+            
+            System.out.println();
+            
+            if(i<(n/2)+1)
+            {
+                st--;
+                sp+=2;
+            }
+            else
+            {
+                st++;
+                sp-=2;
+            }
+            
+        }
+        
+        
+        
+
+    }
+}
