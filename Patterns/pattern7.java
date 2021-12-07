38a54f5502bf39467b18a057c2fe94e800ca47c2
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2021-12-07 21:36:54 +0530

    Create pattern7.java

---
+++ Patterns/pattern7.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        int sp = 0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            
+            for(int space = 1; space<=sp; space++)
+            {
+                System.out.print("	");
+            }
+            
+            System.out.print("*	");
+            
+            System.out.println();
+            sp++;
+        }
+
+    }
+}
