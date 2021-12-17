05360dff96c3907f74a19c5188c3b74b7f5c9943
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2021-12-17 22:26:45 +0530

    Create Pattern14.java

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,27 @@
+/*3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21*/
+
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n + " * " + i + " = "+n*i);
+        }
+
+    }
+}
